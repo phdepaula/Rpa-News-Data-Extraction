6974810c74123d43bf42f08908cc7bca175f31a9
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2024-08-30 10:59:49 -0300

    fix: Updating main to execute task

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("main")
+from tasks.task_robot_work_items import TaskRobotWorkItems
+
+if __name__ == "__main__":
+    tasks = [TaskRobotWorkItems]
+
+    for task in tasks:
+        instance = task()
+        instance.execute_task()
+
+        result = instance.results
+        print(result)
