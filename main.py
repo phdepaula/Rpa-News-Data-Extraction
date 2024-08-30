40befe31baabdc66bdf4f498e17ffbe3ff3324d1
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2024-08-30 16:40:14 -0300

    fix: Fixing tasks execution proccess

--- main.py
+++ main.py
@@ -1,16 +1,43 @@
 from tasks.task_robot_work_items import TaskRobotWorkItems
+from tasks.task_news_data_extraction import TaskNewsDataExtraction
 
 from util.error_manager import ErrorManager
 
 
+# Tasks
+ROBOT_WORK_ITEMS = "Robot Work Items"
+NEWS_DATA_EXTRACTION = "News Data Extraction"
+
+# General Constants
+INSTANCE = "Instance"
+RESULTS = "Results"
+
+
 if __name__ == "__main__":
-    tasks = [TaskRobotWorkItems]
+    tasks = {
+        ROBOT_WORK_ITEMS: {
+            INSTANCE: None,
+            RESULTS: None,
+        },
+        NEWS_DATA_EXTRACTION: {
+            INSTANCE: None,
+            RESULTS: None,
+        },
+    }
 
-    for task in tasks:
-        instance = task()
+    for task, task_information in tasks.items():
         try:
-            instance.execute_task()
+            instance = None
 
-            result = instance.results
+            if task == ROBOT_WORK_ITEMS:
+                instance = TaskRobotWorkItems()
+            elif task == NEWS_DATA_EXTRACTION:
+                input_data = tasks[ROBOT_WORK_ITEMS][RESULTS]
+                instance = TaskNewsDataExtraction(input_data)
+
+            instance.execute_task()
+            task_information.update(
+                {INSTANCE: instance, RESULTS: instance.results}
+            )
         except ErrorManager as error:
             print(error.get_error_description())
