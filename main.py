8ead7fff02edd74a35045fd658994bd730c64e59
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2024-08-31 18:37:05 -0300

    feat: Including new task to main

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
-from tasks.task_robot_work_items import TaskRobotWorkItems
+from tasks.task_excel_report_generator import TaskExcelReportGenerator
 from tasks.task_news_data_extraction import TaskNewsDataExtraction
+from tasks.task_robot_work_items import TaskRobotWorkItems
 
 from util.error_manager import ErrorManager
-
 
 # Tasks
 ROBOT_WORK_ITEMS = "Robot Work Items"
 NEWS_DATA_EXTRACTION = "News Data Extraction"
+EXCEL_REPORT_GENERATOR = "Excel Report Generator"
 
 # General Constants
 INSTANCE = "Instance"
@@ -23,6 +24,10 @@ if __name__ == "__main__":
             INSTANCE: None,
             RESULTS: None,
         },
+        EXCEL_REPORT_GENERATOR: {
+            INSTANCE: None,
+            RESULTS: None,
+        },
     }
 
     for task, task_information in tasks.items():
@@ -34,10 +39,16 @@ if __name__ == "__main__":
             elif task == NEWS_DATA_EXTRACTION:
                 input_data = tasks[ROBOT_WORK_ITEMS][RESULTS]
                 instance = TaskNewsDataExtraction(input_data)
+            elif task == EXCEL_REPORT_GENERATOR:
+                input_data = (
+                    tasks[ROBOT_WORK_ITEMS][RESULTS]
+                    | tasks[NEWS_DATA_EXTRACTION][RESULTS]
+                )
+                instance = TaskExcelReportGenerator(input_data)
 
             instance.execute_task()
             task_information.update(
                 {INSTANCE: instance, RESULTS: instance.get_results()}
             )
-        except ErrorManager as error:
-            print(error.get_error_description())
+        except ErrorManager as error_manager:
+            print(error_manager.get_error_description())
