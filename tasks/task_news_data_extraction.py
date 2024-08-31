3fdaab86c88831a030acff5919f7ebd14852137e
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2024-08-31 17:31:25 -0300

    feat: Adding getting articles information

--- tasks/task_news_data_extraction.py
+++ tasks/task_news_data_extraction.py
@@ -15,8 +15,13 @@ class TaskNewsDataExtraction(Task):
 
     SEARCH_PHRASE = "search_phrase"
     SORT_BY = "sort_by"
+    TITLE = "title"
+    DESCRIPTION = "description"
+    DATE = "date"
+    IMAGE_SRC = "image_src"
 
     def __init__(self, input_data: Dict) -> None:
+        super().__init__()
         self.input_data = input_data
         self.__url = "https://www.aljazeera.com/"
         self.__selenium_handler = SeleniumHandler(self.__url)
@@ -26,6 +31,59 @@ class TaskNewsDataExtraction(Task):
         Method responsible for closing browser.
         """
         self.__selenium_handler.close_browser()
+
+    def _get_articles_information(self) -> None:
+        """
+        Method responsible for getting information
+        from all articles.
+        """
+        articles_path = (
+            """//*[@id="main-content-area"]/div[2]/div[2]/article"""
+        )
+        articles_elements = self.__selenium_handler.find_elements(
+            articles_path
+        )
+        quantity_articles = len(articles_elements)
+
+        position = 1
+
+        while position <= quantity_articles:
+            title_path = (
+                articles_path + f"[{position}]/div[2]/div[1]/h3/a/span"
+            )
+            title_text = self.__selenium_handler.get_element_text(title_path)
+
+            description_path = (
+                articles_path + f"[{position}]/div[2]/div[2]/div/p"
+            )
+            description_text = self.__selenium_handler.get_element_text(
+                description_path
+            )
+
+            date_path = (
+                articles_path
+                + f"[{position}]/div[2]/footer/div/div/div/div/span[2]"
+            )
+            date_text = self.__selenium_handler.get_element_text(date_path)
+
+            image_path = articles_path + f"[{position}]/div[3]/div/div/img"
+            attribute = "src"
+            image_src = self.__selenium_handler.get_element_attribute(
+                image_path, attribute
+            )
+
+            self._update_results(
+                {
+                    str(position): {
+                        self.TITLE: title_text,
+                        self.DESCRIPTION: description_text,
+                        self.DATE: date_text,
+                        self.IMAGE_SRC: image_src,
+                    }
+                }
+            )
+
+            position += 1
 
     def _open_browser(self) -> None:
         """
@@ -92,12 +150,13 @@ class TaskNewsDataExtraction(Task):
             self._wait_for_page_to_load(2)
             self._sort_news()
             self._wait_for_page_to_load(2)
+            self._get_articles_information()
+            self._wait_for_page_to_load(2)
             self._close_browser()
-            self._update_results(["results"])
         except ErrorManager as error_manager:
             raise error_manager
         except Exception as error:
             message = f"Error executing task News Data Extraction: {error}"
-            error_code = 11
+            error_code = 30
 
             raise ErrorManager(message, error_code)
