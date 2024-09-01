3a566475fdbbe7837d506daa57f3972778b19c2d
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2024-09-01 14:55:03 -0300

    feat: Including new features to use news images

--- tasks/task_excel_report_generator.py
+++ tasks/task_excel_report_generator.py
@@ -1,4 +1,5 @@
 import re
+import os
 from datetime import datetime
 from typing import Dict, List
 
@@ -6,6 +7,7 @@ from drivers.excel_generator import ExcelGenerator
 from tasks.task import Task
 
 from util.error_manager import ErrorManager
+import requests
 
 
 class TaskExcelReportGenerator(Task):
@@ -20,12 +22,14 @@ class TaskExcelReportGenerator(Task):
     DATE = "date"
     IMAGE_SRC = "image_src"
     COLUMNS = "columns"
+    OUTPUT = "output"
 
     def __init__(self, robot_data: Dict, news_data: Dict) -> None:
         super().__init__()
         self.robot_data = robot_data
         self.news_data = news_data
         self.__excel_generator = ExcelGenerator()
+        self.__excel_path = None
 
     def _check_if_contains_monetary_value(self, text: str) -> bool:
         """
@@ -66,11 +70,35 @@ class TaskExcelReportGenerator(Task):
                 "Picture Filename",
                 "Occurrence of Search Phrase in Title and Description",
                 "Title and Description Have Monetary Value?",
+                "News Image",
             ]
         ]
         self.__excel_generator.add_data(columns)
 
         self._update_results({self.COLUMNS: columns})
+
+    def _download_image(self, url: str, name: str) -> None:
+        """
+        Method responsible for download an image
+        to the output directory.
+        """
+        try:
+            response = requests.get(url)
+
+            if response.status_code == 200:
+                path = f"{self.OUTPUT}/{name}.jpg"
+
+                with open(path, "wb") as image:
+                    image.write(response.content)
+        except Exception:
+            print(f"Failed to download image {name}.")
+
+    def _format_excel_file(self) -> None:
+        """
+        Method responsible for formatting excel file.
+        """
+        self.__excel_generator.set_row_height(self.__excel_path, 135)
+        self.__excel_generator.adjust_width_to_show_data(self.__excel_path)
 
     def _generate_report_data(self) -> List:
         """
@@ -78,8 +106,8 @@ class TaskExcelReportGenerator(Task):
         """
         for key, data in self.news_data.items():
             title = self._treat_text(data.get(self.TITLE, ""))
-            date = self._treat_date(data.get(self.DATE, ""))
             description = self._treat_text(data.get(self.DESCRIPTION, ""))
+            date = self._treat_date(data.get(self.DATE, ""))
             picture_filename = data.get(self.IMAGE_SRC, "")
 
             combined_text = title + " " + description
@@ -107,14 +135,32 @@ class TaskExcelReportGenerator(Task):
 
             self.__excel_generator.add_data(data_result)
             self._update_results({key: data_result})
+            self._download_image(picture_filename, key)
+
+    def _include_news_image(self) -> None:
+        """
+        Method responsible for including
+        the news image inside the report.
+        """
+        for key in self.news_data.keys():
+            image_path = f"{self.OUTPUT}/{key}.jpg"
+            cell = f"G{int(key) + 1}"
+
+            image_added = self.__excel_generator.add_image_to_cell(
+                image_path, cell, self.__excel_path
+            )
+
+            if image_added is True:
+                os.remove(image_path)
 
     def _save_file(self) -> None:
         """
         Method responsible for saving the excel file.
         """
         current_date = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-        path = f"output/report_news_{current_date}.xlsx"
-        self.__excel_generator.save_workbook(path)
+        self.__excel_path = f"{self.OUTPUT}/report_news_{current_date}.xlsx"
+
+        self.__excel_generator.save_workbook(self.__excel_path)
 
     def _treat_date(self, date: str) -> str:
         """
@@ -153,6 +199,8 @@ class TaskExcelReportGenerator(Task):
             self._create_template()
             self._generate_report_data()
             self._save_file()
+            self._include_news_image()
+            self._format_excel_file()
         except ErrorManager as error_manager:
             raise error_manager
         except Exception as error:
