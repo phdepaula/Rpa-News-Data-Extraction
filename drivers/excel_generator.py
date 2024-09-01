05231a59e8fa263eb52603970b92bbf5722635dd
Pedro Henrique de Paula Alves <pedrohenriquedepaulaalves@yahoo.com.br>
2024-09-01 14:54:07 -0300

    feat: Including new methods to excel generator

--- drivers/excel_generator.py
+++ drivers/excel_generator.py
@@ -1,5 +1,9 @@
+import os
 from typing import List
 
+from openpyxl import load_workbook
+from openpyxl.drawing.image import Image
+from openpyxl.utils import get_column_letter
 from RPA.Excel.Files import Files
 
 from util.error_manager import ErrorManager
@@ -27,6 +31,62 @@ class ExcelGenerator:
 
             raise ErrorManager(message, error_code)
 
+    def adjust_width_to_show_data(self, excel_path: str) -> None:
+        """
+        Method responsible for defining the width of the columns.
+        """
+        try:
+            work_book = load_workbook(excel_path)
+            work_sheet = work_book.active
+
+            for column in work_sheet.columns:
+                max_length = 0
+                column_letter = get_column_letter(column[0].column)
+
+                for cell in column:
+                    try:
+                        if cell.value:
+                            max_length = max(max_length, len(str(cell.value)))
+                    except Exception:
+                        pass
+
+                adjusted_width = max_length + 2
+                work_sheet.column_dimensions[column_letter].width = (
+                    adjusted_width
+                )
+
+            work_book.save(excel_path)
+        except Exception as error:
+            message = f"Error adjusting width: {error}"
+            error_code = 12
+
+            raise ErrorManager(message, error_code)
+
+    def add_image_to_cell(
+        self, image_path: str, cell: str, excel_path: str
+    ) -> bool:
+        """
+        Method responsible for adding image to a
+        cell in a excel file.
+        """
+        try:
+            if os.path.isfile(image_path) and os.path.isfile(excel_path):
+                work_book = load_workbook(excel_path)
+                work_sheet = work_book.active
+                image = Image(image_path)
+
+                work_sheet.add_image(image, cell)
+                work_book.save(excel_path)
+
+                return True
+
+            return False
+        except Exception as error:
+            message = f"Error adding image to cell: {error}"
+            error_code = 13
+
+            raise ErrorManager(message, error_code)
+
     def create_workbook(self) -> None:
         """
         Method responsible for creating
@@ -36,7 +96,7 @@ class ExcelGenerator:
             self.file.create_workbook()
         except Exception as error:
             message = f"Error creating workbook: {error}"
-            error_code = 12
+            error_code = 14
 
             raise ErrorManager(message, error_code)
 
@@ -49,6 +109,24 @@ class ExcelGenerator:
             self.file.save_workbook(path)
         except Exception as error:
             message = f"Error creating workbook: {error}"
-            error_code = 12
+            error_code = 15
+
+            raise ErrorManager(message, error_code)
+
+    def set_row_height(self, excel_path: str, height: float) -> None:
+        """
+        Method responsible for defining the height of the lines.
+        """
+        try:
+            work_book = load_workbook(excel_path)
+            work_sheet = work_book.active
+
+            for row in work_sheet.iter_rows(min_row=2):
+                work_sheet.row_dimensions[row[0].row].height = height
+
+            work_book.save(excel_path)
+        except Exception as error:
+            message = f"Error setting row height: {error}"
+            error_code = 16
 
             raise ErrorManager(message, error_code)
